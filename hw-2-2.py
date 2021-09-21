fc6f979db0bd856f3b466f1947b1d0d37cdacc5f
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-09-21 10:19:44 -0400

    Add file

---
+++ hw-2-2.py
@@ -0,0 +1,9 @@
+# Author: JRI 9/21/21
+
+free_throws_scored = 1 
+regular_baskets_scored = 1
+three_pointers_scored = 1
+
+total = (1 * free_throws_scored) + (2 * regular_baskets_scored) + (3 * three_pointers_scored)
+print(total)
+
